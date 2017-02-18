de28866c11cefe024a46adcd4ef6c052fe78e448
Lorena González <lore_383@hotmail.com>
2017-02-19 00:12:04 +0100

    Añadida petición a la api de la wikipedia, sin especificación de datos a recoger.

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,10 +1,10 @@
-var ENDPOINT = "data/band.json";
+var BANDS = "data/band.json";
 var PAGES = "data/pages.json";
 var SLIDES = "data/slides.json";
 
 var findBand = function (array, id){
   for (var i = 0, m = null; i<array.length; i++){
-    if (array[i].id == id) {
+    if (array[i].path == id) {
       return array[i];
     };
   };
@@ -24,18 +24,67 @@ function menuController($scope, $route, $http){
 };
 
 function bandController($scope, $route, $routeParams, $http){
+	var bandSelected = "";
 	$scope.$route = $route;
-	$scope.band_Id = $routeParams.bandId;
-	$http.get(url).then(function(id){
-		var band = id.data;
-		$scope.bands = findBand(band, $scope.band_Id);
-	});
+	$scope.band_Id = $routeParams.id;
+	
+
+	function getBandSelected(){
+		return new Promise( function(resolve, reject) {
+			$http.get(BANDS).then(function(id){
+				var band = id.data;
+				$scope.bands = findBand(band, $scope.band_Id);
+				bandSelected = $scope.bands.wiki;
+				if ($scope.bands) {
+					resolve($scope.bands);
+				} else {
+					var error = 'No se han encontrado resultados';
+					reject(error);
+				}
+				
+				
+			});
+		})
+	}
+	getBandSelected()
+	.then(function(){
+		
+		$.ajax({
+			type: "GET",
+			url: "https://es.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&rvprop=content&titles=" + bandSelected,	
+				
+			success: function(response){
+				$scope.bands.text = response.query;
+				var pages = response.query.pages;
+				
+			},
+			error: function(error){
+				console.error('No se han encontrado resultados.', error);
+			},
+			dataType: "JSON" 
+		});
+	})
+	.catch(function () {
+		console.log('Error');
+	})
+
+	//Wiki API 
+
+	// http://en.wikipedia.org/w/api.php?format=json&action=query
+	// titles=PáginaA|PáginaB|PáginaC
+
+	//Imagen:
+	// http://es.wikipedia.org/w/api.php?format=json&action=query&titles=Archivo:Gandhi_Nehru_Indira.jpg&prop=imageinfo&iiprop=url|size
+	// respuesta.query.pages[-1].imageinfo[0].url
+
 };
 
 
 (function() {
 	var ctrl = angular.module('myApp.controllers', []);
-	ctrl.controller('homeController', homeController);
+	ctrl.controller('homeController', function (){});
+	ctrl.controller('fooController', function (){});
+	ctrl.controller('barController', function (){});
 	ctrl.controller('menuController', menuController);
 	ctrl.controller('bandController', bandController);
 })();
